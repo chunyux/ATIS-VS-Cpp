01899ec150580ce6b62ef53e1977c65034491d03
chunyu <chunyux@users.noreply.github.com>
2015-06-26 17:27:38 +0200

    Update SingleEvent.cpp
    Optimisation of code with LU

--- SingleEvent.cpp
+++ SingleEvent.cpp
@@ -1,12 +1,11 @@
-#include "SingleEvent.h"
+#include "stdafx.h"
 
+SingleEvent::SingleEvent(short p, short x, short y, int t): 
+polarity(p), x_coordinate(x), y_coordinate(y), t_stamp(t) 
+{ 
+} 
 
-SingleEvent::SingleEvent(int p, int x, int y, int t):
-polarity(p), x_coordinate(x), y_coordinate(y), t_stamp(t)
-{
-}
-
-
-SingleEvent::~SingleEvent()
-{
-}
+ 
+SingleEvent::~SingleEvent() 
+{ 
+} 
