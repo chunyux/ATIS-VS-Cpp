39dfe50ca527fbcda4f012f85b7c15dd4651d926
chunyu <chunyux@users.noreply.github.com>
2015-06-29 17:55:18 +0200

    Update stdafx.h

--- stdafx.h
+++ stdafx.h
@@ -1,3 +1,8 @@
+// stdafx.h : fichier Include pour les fichiers Include système standard,
+// ou les fichiers Include spécifiques aux projets qui sont utilisés fréquemment,
+// et sont rarement modifiés
+//	@Created by Chunyu XIANG 03/2015	
+
 #pragma once
 #include <stdio.h>
 #include <tchar.h>
@@ -5,3 +10,9 @@
 #include "targetver.h"
 #include "SingleEvent.h" 
 #include "EventMatrix.h"
+#include "AtisEventBuffer.h"
+
+
+
+
+// TODO : faites référence ici aux en-têtes supplémentaires nécessaires au programme
