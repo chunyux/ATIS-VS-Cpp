63beada23973a1a518411f3e517e8cb6800e38bb
chunyu <chunyux@users.noreply.github.com>
2015-06-29 17:51:59 +0200

    Update EventMatrix.h
    Add some comments, add clearMatrix(), buffer2matrix(AtisEventBuffer&)

--- EventMatrix.h
+++ EventMatrix.h
@@ -1,19 +1,23 @@
+//	@Created by Chunyu XIANG 03/2015	
 #pragma once
 #include "DataList.h"
+#include "AtisEventBuffer.h"
 
 class EventMatrix
 {
 private:
-	int m[HEIGHT][WIDTH];
+	int m[HEIGHT][WIDTH]; //Atis 304*240 matrix
 public:
 	
 	EventMatrix();
-	EventMatrix(SingleEvent*);
-
+	void buffer2matrix(AtisEventBuffer&); 
 	void matrix2txt();
+	void clearMatrix();
 	~EventMatrix();
 
 };
+
+
 
 
 
