e850c81c6b31a0ceef5691d440187d6dfb78739b
chunyu <chunyux@users.noreply.github.com>
2015-06-29 17:48:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,44 +1,49 @@
-//#include <iostream>
+// This is the main entrance of program where we
+// 1. read the txt(input)
+// 2. load all events in txt into a list of events
+// 3. load certain number of events(bufferLength) into AtisEventBuffer
+// 4. put events from buffer into a 304*240 matrix
+// 5. save matrix into another txt(as output)
+// 6. update buffer(updateLength)
+
+//	@Created by Chunyu XIANG 03/2015							
+
 #include "stdafx.h"
 #include <deque>
-//#include "champMarkov.h"
-//#include "EventMatrix.h"
-//#include <opencv2\opencv.hpp>
 
 using namespace std;
-//using namespace cv;
 
 const int bufferLength = 2000;
-const int updateLength = 1000;
+const unsigned short updateLength = 1000;
 
 int main(){
+	// 1. we read the txt
 	DataList dataLoader("C://Users//xiang//Documents//Visual Studio 2008//Projects//helloWorld//helloWorld//eventRecord_28_04_15_18_10_08.txt");
-	dataLoader.load(); //no parameter **interface designe [CHECKED]
-	deque <SingleEvent> EventBuffer;
-	list <SingleEvent>::iterator iterEventList;
-
-	for(int ibufferLength = 0,iterEventList = dataLoader.eventList.begin(); ibufferLength <bufferLength ;ibufferLength ++,iterEventList++){
-			EventBuffer.push_back(*iterEventList);
-	}
+	// 2. load all events in txt into a list of events
+	dataLoader.load(); 
+	AtisEventBuffer* pEventBuffer = new AtisEventBuffer(bufferLength);
+	// 3. Initialise buffer and matrix
+	pEventBuffer->init(dataLoader.eventList);
+	EventMatrix* pEventMatrix = new EventMatrix();
 
 	while (1){
 
-		//push back 2000
-		for(int iupdateLength = 0; iupdateLength < updateLength ;iupdateLength++,iterEventList++){
-			EventBuffer.push_back(*iterEventList);
+		if(pEventBuffer->isInitialised){
+			remove("C://Users//xiang//Documents//Visual Studio 2008//Projects//helloWorld//helloWorld//Output.txt"); 
+			pEventMatrix->clearMatrix();
+
+			// 4. put events from buffer into a 304*240 matrix
+			pEventMatrix->buffer2matrix(*pEventBuffer);
+			// 5. save matrix into another txt(as output)
+			pEventMatrix->matrix2txt();
+			// 6. update buffer(updateLength)
+			pEventBuffer->update(updateLength);
 		}
-
-		EventMatrix* pEventMatrix = new EventMatrix(dataLoader);
-		
-
-		pEventMatrix->matrix2txt();
-
-		//pop_front 1000 events
-		for(int iupdateLength = 0; iupdateLength < updateLength; iupdateLength ++){
-			EventBuffer.pop_front();
-		}
-		delete pEventMatrix;
+		else
+			cout<< "Error: Buffer not initialised!" << endl;
 
 	}
+
+	delete pEventMatrix;
 	return 1;
 }
